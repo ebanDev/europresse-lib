46f5c9b8c63331822eb962430010a2b62077d3c3
Eban <account+github@eban.eu.org>
2024-02-05 16:53:26 +0100

    Revert "[🐛 Fix] Update ent-cookies version"
    This reverts commit 0522b2fa824d94f77cd7c5c3b207abd4e874a423.

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { fetch, CookieJar } from "npm:node-fetch-cookies";
 export { JSDOM, Element } from "npm:jsdom";
-export { getCookies } from "https://deno.land/x/ent_cookies@v1.0.3/mod.ts";
+export { getCookies } from "https://deno.land/x/ent_cookies@v1.0.2/mod.ts";
